3e3246b3e1d678436deaf3fa1e62ce4d90d3a534
Abraham Salazar <asalazarmu@proveedor.intercorp.com.pe>
2021-12-01 20:37:22 -0500

    feat: Add purchase function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+declare const fbq: any; // 1. add an ambient declaration
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'twb-front';
+
+  purchase() {
+    fbq('track', 'Purchase', { currency: 'USD', value: 30.0 });
+  }
 }
