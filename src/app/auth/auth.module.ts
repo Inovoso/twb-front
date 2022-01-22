8c210f6bba672f3e1a1185c5502e05e3464eb517
Abraham Salazar <asalazarmu@proveedor.intercorp.com.pe>
2022-01-22 12:17:12 -0500

    feat: Create basic login page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,8 +7,20 @@ import { RegisterComponent } from './register/register.component';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-const MATERIAL_MODULES = [MatToolbarModule];
+const MATERIAL_MODULES = [
+  MatToolbarModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+];
+const FORM_MODULES = [FormsModule, ReactiveFormsModule];
 @NgModule({
   declarations: [
     LoginComponent,
@@ -16,6 +28,11 @@ const MATERIAL_MODULES = [MatToolbarModule];
     ToolbarComponent,
     FooterComponent,
   ],
-  imports: [CommonModule, AuthRoutingModule, ...MATERIAL_MODULES],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ...MATERIAL_MODULES,
+    ...FORM_MODULES,
+  ],
 })
 export class AuthModule {}
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  mainForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.mainForm = this.fb.group({
+      email: [''],
+      password: [''],
+    });
   }
 
+  ngOnInit(): void {}
 }
