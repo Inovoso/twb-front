c8bd0d24fdb06dc5bd468b1e8279635157c2856b
Abraham Salazar <asalazarmu@proveedor.intercorp.com.pe>
2022-01-22 10:03:34 -0500

    chore: Add material modules array for Auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,9 +6,16 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+const MATERIAL_MODULES = [MatToolbarModule];
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, ToolbarComponent, FooterComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ToolbarComponent,
+    FooterComponent,
+  ],
+  imports: [CommonModule, AuthRoutingModule, ...MATERIAL_MODULES],
 })
 export class AuthModule {}
